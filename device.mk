1c4405c36892ac92240f7beb01b17e3525537af1
Rahul Krishna <rahulkrishna585@gmail.com>
2020-05-24 18:15:27 +0000

    X01BD: Get rid of android.hidl.base@1.0.so dependency
    * libhidlbase.so is used instead
    * Remove shims for it
    
    Change-Id: I5d0e5ca2543d2ea3fae81c5a0aadbce51e52690c

--- device.mk
+++ device.mk
@@ -271,13 +271,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
-
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
